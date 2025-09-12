322b716bce1db849211720a1b5dd7db35ac5192a
lucagior22 <lucagior22@gmail.com>
2025-09-12 15:14:10 -0300

    tp3 init

---
+++ practicas/src/TP3/ejerciciouno/Stack.java
@@ -0,0 +1,9 @@
+package TP3.ejerciciouno;
+
+public class Stack {
+    private java.util.ArrayList items;
+    public Stack() {}
+    public void push(Object item) {}
+    public Object pop() {}
+    public boolean isEmpty() {}
+}
